7897af9f5ee72d100c23665a03766ddd15bfbcd6
Andruraj <aamalraj@plexustg.com>
2023-01-27 18:49:07 +0530

    Simple Button

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen w-full bg-neutral-800 text-white">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        Advanced Button
+      </div>
+    </div>
+  );
+}
